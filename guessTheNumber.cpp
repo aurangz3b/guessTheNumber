12e1f99939af1764e1ca8c21f94287d3f8fec09e
aurangz3b <2023nitsgr735@nitsri.ac.in>
2024-07-21 11:09:14 +0530

    Add files via upload

---
+++ guessTheNumber.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+int randomNumber, guessedNumber;
+int attempts = 0;
+// generating a random number
+    // srand(static_cast<unsigned>(time(0)));
+int x = time(0);
+srand(x);
+randomNumber = 1 + rand() % 99;
+//loop
+
+while(true){
+cout << "enter the gussed number: "<<endl;
+cin >> guessedNumber;
+attempts++;
+if(guessedNumber > randomNumber)
+{
+    cout << "your guess is high, aim lower"<<endl;
+}
+else if (guessedNumber < randomNumber)
+{
+    cout << "your guess is low, aim higher"<<endl;
+}
+else
+{
+cout << "that is right , you guessed it right"<< endl;
+break;
+}
+}
+cout << "you guessed the number in: "<< attempts <<" attempts";
+return 0;
+}
